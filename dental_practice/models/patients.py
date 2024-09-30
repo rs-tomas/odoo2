9a497bd98ce494bc72bc96f687815c3ee8668c43
Federico Torres <federico.yakusik@twonary.com>
2024-09-30 07:23:42 -0400

    commenting dental.xml

--- dental_practice/models/patients.py
+++ dental_practice/models/patients.py
@@ -13,8 +13,6 @@ class OdooExpPatient(models.Model):
         ('other', 'Other'),
     ], string='Gender')
     medical_history = fields.Text(string="Medical History")
-    intervention_ids = fields.One2many('odooexp.intervention', 'patient_id', string='Interventions')
-    treatment_ids = fields.One2many('odooexp.treatment', 'patient_id', string='Treatments')
     # Add a computed field to calculate the age of the patient
     age = fields.Integer(string='Age', compute='_compute_age', store=True)
     # inverse field for the patiend_id of interventions and treatments
