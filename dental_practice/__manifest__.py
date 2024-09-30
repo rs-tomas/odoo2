9dab285bb20d8b912809364c1623ed9e1e5b2e42
unknown <fernandez@rising-systems.de>
2024-09-30 16:04:04 +0200

    inherit users and add data

--- dental_practice/__manifest__.py
+++ dental_practice/__manifest__.py
@@ -16,6 +16,8 @@
         #'views/patients/patients_tree_view.xml',
         'views/treatments/treatments_form_view.xml',
         'views/treatments/treatments_tree_view.xml',
+        'data/dental_data.xml',
+        'views/extends/extend_res_partner.xml'
     ],
     'assets': {
         'web.assets_backend': [
