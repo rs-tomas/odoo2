b12a31dba36d5c9f90f55bbb435ac657e4a9b039
unknown <fernandez@rising-systems.de>
2024-09-30 11:29:22 +0200

    test

--- dental_practice/__manifest__.py
+++ dental_practice/__manifest__.py
@@ -9,7 +9,13 @@
     'depends': ['base', 'mail'],
     'data': [
         'views/dental.xml',
-        'security/ir.model.access.csv'
+        'security/ir.model.access.csv',
+        'views/interventions/interventions_form_view.xml',
+        'views/interventions/interventions_tree_view.xml',
+        'views/patients/patients_form_view.xml',
+        'views/patients/patients_tree_view.xml',
+        'views/treatments/treatments_form_view.xml',
+        'views/treatments/treatments_tree_view.xml',
     ],
     'assets': {
         'web.assets_backend': [
