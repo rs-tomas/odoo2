842a24331d906de18f65b6f2a61fe6ceb5035ed2
Federico Torres <federico.yakusik@twonary.com>
2024-09-30 05:51:21 -0400

    modifying dental and interventions models

--- dental_practice/models/interventions.py
+++ dental_practice/models/interventions.py
@@ -3,14 +3,6 @@ from odoo import api, fields, models
 from odoo.http import request
 
 class OdooExpInterventions(models.Model):
-    _name = 'odooexp.interventions'
-    _description = 'empty model for odooexp.interventions'
-    _inherit = ['mail.thread', 'mail.activity.mixin']
-    _order = 'id desc'
-
-    name = fields.Char(string='Name')
-
-class DentalIntervention(models.Model):
     _name = 'dental.intervention'
     _description = 'Dental Intervention'
     _inherit = ['mail.thread', 'mail.activity.mixin']
--- dental_practice/models/patients.py
+++ dental_practice/models/patients.py
@@ -2,17 +2,6 @@
 from odoo import api, fields, models
 from odoo.http import request
 
-class OdooExpPatients(models.Model):
-    _name = 'odooexp.patients'
-    _description = 'empty model for odooexp.patients'
-    _inherit = ['mail.thread', 'mail.activity.mixin']
-    _order = 'id desc'
-
-    name = fields.Char(string='Name')
-    medical_history = fields.Text(string='Medical History')
-    intervention_ids = fields.One2many('odooexp.intervention', 'patient_id', string='Interventions')
-    treatment_ids = fields.One2many('odooexp.treatment', 'patient_id', string='Treatments')
-
 class OdooExpPatient(models.Model):
     _inherit = 'res.partner'  # Inherit from the existing 'res.partner' model
 
@@ -26,27 +15,14 @@ class OdooExpPatient(models.Model):
     medical_history = fields.Text(string="Medical History")
     intervention_ids = fields.One2many('odooexp.intervention', 'patient_id', string='Interventions')
     treatment_ids = fields.One2many('odooexp.treatment', 'patient_id', string='Treatments')
+    # Add a computed field to calculate the age of the patient
+    age = fields.Integer(string='Age', compute='_compute_age', store=True)
 
-
-
-# class OdooExpPatient(models.Model):
-#     _name = 'odooexp.patient'
-#     _description = 'Patient Information'
-#     _inherit = ['mail.thread', 'mail.activity.mixin']
-#     _order = 'id desc'
-
-#     name = fields.Char(string='Full Name', required=True)
-#     birth_date = fields.Date(string='Birth Date')
-#     gender = fields.Selection([
-#         ('male', 'Male'),
-#         ('female', 'Female'),
-#         ('other', 'Other'),
-#     ], string='Gender')
-#     phone = fields.Char(string='Phone')
-#     email = fields.Char(string='Email')
-#     address = fields.Char(string='Address')
-#     medical_history = fields.Text(string='Medical History')
-#     intervention_ids = fields.One2many('odooexp.intervention', 'patient_id', string='Interventions')
-#     treatment_ids = fields.One2many('odooexp.treatment', 'patient_id', string='Treatments')
-
-
+    @api.depends('birth_date')
+    def _compute_age(self):
+        for record in self:
+            if record.birth_date:
+                record.age = fields.Date.from_string(fields.Date.today()).year - fields.Date.from_string(record.birth_date).year
+            else:
+                record.age = 0
+    
