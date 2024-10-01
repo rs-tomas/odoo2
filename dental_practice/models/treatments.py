10267b362ef3689b209b5d09e2a04473a4238524
Sergei Baryshevskii <black3knight@gmail.com>
2024-10-01 09:42:39 +0200

    Add dental tooth model

--- dental_practice/models/treatments.py
+++ dental_practice/models/treatments.py
@@ -18,3 +18,20 @@ class DentalTreatment(models.Model):
     # invoice_id = fields.Many2one('account.move', string='Invoice', readonly=True)
     interventions_ids = fields.Many2many('dental.intervention', string='Interventions')
     sales_order_id = fields.Many2one('sale.order', string='Sales Order', readonly=True)
+    profile = fields.Selection([("Therapy", "Therapy"), ("Surgery", "Surgery"), ("Orthodontics", "Orthodontics")], string='Treatment Profile')
+    teeth = fields.Many2many("dental.tooth", string='Teeth for treatment')
+
+
+
+class Tooth(models.Model):
+    _name = 'dental.tooth'
+    _description = 'Tooth, according to the standard numeration'
+
+    quarter = fields.Selection([("1", "1"), ("2", "2"), ("3", "3"), ("4", "4")], string='Quarter')
+    tooth = fields.Selection([("1", "1"), ("2", "2"), ("3", "3"), ("4", "4"), ("5", "5"), ("6", "6"), ("7", "7"), ("8", "8")])
+    tooth_number = fields.Integer(string='Tooth Number', compute='_compute_tooth_number')
+
+    @api.depends('quarter', 'tooth')
+    def _compute_tooth_number(self):
+        for record in self:
+            record.tooth_number = int(record.quarter) * 10 + int(record.tooth)
