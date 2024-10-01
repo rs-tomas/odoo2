ea3ff1706a8f1c276ce9d6ea4eb6bb43ddd67a6c
Sergei Baryshevskii <black3knight@gmail.com>
2024-10-01 13:26:58 +0200

    Move tooth model to intervations

--- dental_practice/models/interventions.py
+++ dental_practice/models/interventions.py
@@ -14,3 +14,17 @@ class OdooExpInterventions(models.Model):
     # using product.product for Interventions and Treatments
     product_id = fields.Many2one('product.product', string='Intervention Product')
     treatment_id = fields.Many2many('dental.treatment', string='Intervention')
+    teeth = fields.Many2many("dental.tooth", string='Teeth for treatment')
+
+class Tooth(models.Model):
+    _name = 'dental.tooth'
+    _description = 'Tooth, according to the standard numeration'
+
+    quarter = fields.Selection([("1", "1"), ("2", "2"), ("3", "3"), ("4", "4")], string='Quarter')
+    position = fields.Selection([("1", "1"), ("2", "2"), ("3", "3"), ("4", "4"), ("5", "5"), ("6", "6"), ("7", "7"), ("8", "8")], string='Position')
+    name = fields.Char(string='Tooth Number', compute='_compute_tooth_name')
+
+    @api.depends('quarter', 'position')
+    def _compute_tooth_name(self):
+        for record in self:
+            record.name = f'{record.quarter}{record.position}'
